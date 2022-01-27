5b83e8f4d8db379223211f489ee874bbf9b97549
Erik Novales <enovales@codehitmen.com>
2022-01-26 23:03:46 -0800

    update to more modern fable

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,18 @@
+// Note this only includes basic configuration for development mode.
+// For a more comprehensive configuration check:
+// https://github.com/fable-compiler/webpack-config-template
+
 var path = require("path");
-var webpack = require("webpack");
 
 module.exports = {
-  entry: "./bin/canvas",
-  output: {
-      path: path.join(__dirname, "public"),
-      filename: "bundle.js",
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    port: 8080
-  }
+    mode: "development",
+    entry: "./src/App.fs.js",
+    output: {
+        path: path.join(__dirname, "./public"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 8080,
+    }
 }
